3792d706af94701952a0b3a277d90184cd488afb
Kartikey Garg <56454264+Kartikey-Garg@users.noreply.github.com>
2021-08-14 18:02:44 +0530

    Update Dice_Rolling_Simulator.py

--- Basic Python Projects/Dice_Rolling_Simulator.py
+++ Basic Python Projects/Dice_Rolling_Simulator.py
@@ -7,14 +7,15 @@ import sys
 def usr():
    return random.randint(1,6)
 print("Die Number:", usr())
-while True:
+x=True
+while x:
     usr()
     n = input("Do you want to quit ? (Y/N)\n")
     if n in ['Y', 'y']:
         print("Bye!!")
-        sys.exit()
+        x=False
     elif n in ['N', 'n']:
         print("Die Number:", usr())
     else:
         print("Invalid Input")
-        sys.exit()
+        x=False
