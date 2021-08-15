c31563e12f708cc31265b8d0608e0dc3e4ab5b77
Kartikey Garg <56454264+Kartikey-Garg@users.noreply.github.com>
2021-08-15 13:36:00 +0530

    Update 8_ball_game.py

--- Basic Python Projects/8_ball_game.py
+++ Basic Python Projects/8_ball_game.py
@@ -3,7 +3,7 @@ print("*******************************")
 print("           Welcome")
 print("    THE MAGIC 8 BALL GAME")
 print("*******************************")
-ans = ['My sources say YES','My reply is yes','100% YES','Yes','Yep','NO never','It is certain','Absolutely not','An Immortal say NO','No','My sources say NO','It is up to you','No one knows except ONE','You mortal Yes you can','Even immortals do not know','No you are wrong','My crew left me cause you asked this question','All the best','Never','You are alone']
-Ques = input("Type your question: ")
-Ball = int(input("Choose a ball between 1 to 8: "))
+ans = ['My sources say YES','100% YES','An Immortal said NO','My sources say NO','It is up to you','You mortal Yes you can','Even immortals do not know','My crew left me cause you asked this question']
+input("Type your question: ")
+input("Choose a ball between 1 to 8: ")
 print("Answer:", random.choice(ans))
