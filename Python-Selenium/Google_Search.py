93c503d631fcd6139286e594c9ae345a550d959f
Kartikey Garg <56454264+Kartikey-Garg@users.noreply.github.com>
2025-11-16 11:39:28 +0530

    Integrate ChromeDriverManager for ChromeDriver setup
    Added ChromeDriverManager to manage ChromeDriver installation.

--- Python-Selenium/Google_Search.py
+++ Python-Selenium/Google_Search.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 # Set the path to the ChromeDriver executable using the Service class
